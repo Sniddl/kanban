512b1e9f83ba7491346abaf43dd69e759f635219
Sys <frameworkthegreat@gmail.com>
2017-09-03 20:27:58 +0100

    Setup

--- cache/routes.js
+++ cache/routes.js
@@ -1,3 +1,3 @@
 module.exports = (app) => {
-	app.post('/api/example', require('C:\\Users\\zeb\\Documents\\kanban\\app\\controllers\\home.js').example);
-	app.get('/:amount?', require('C:\\Users\\zeb\\Documents\\kanban\\app\\controllers\\home.js').index);}
+	app.post('/api/example', require('C:\\xampp\\htdocs\\kanban\\app\\controllers\\home.js').example);
+	app.get('/:amount?', require('C:\\xampp\\htdocs\\kanban\\app\\controllers\\home.js').index);}
